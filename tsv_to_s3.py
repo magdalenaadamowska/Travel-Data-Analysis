10fe42cef3df34dd356891e422270bb6346172fb
Magdalena Adamowska <magdalenaadamowska@gmail.com>
2025-04-04 13:34:11 +0200

    pipeline test

--- tsv_to_s3.py
+++ tsv_to_s3.py
@@ -9,7 +9,12 @@ import pandas as pd
 import requests
 import yaml
 
-with open("s3_tourism_credentials.json", "r", encoding="UTF-8") as credentials_file:
+
+CREDENTIALS_FILE = "s3_tourism_credentials.json"
+TSV_LIST_FILENAME = "url_tsv.yml"
+
+print("Loading credentials")
+with open(CREDENTIALS_FILE, "r", encoding="UTF-8") as credentials_file:
     credentials = json.load(credentials_file)
 
 aws_key_id = credentials["aws_key_id"]
@@ -17,23 +22,32 @@ aws_secret_key = credentials["aws_secret_key"]
 bucket_name = credentials["bucket_name"]
 region_name = credentials["region_name"]
 
+print("Connecting to AWS")
 s3 = boto3.client(
     "s3",
     aws_access_key_id=aws_key_id,
     aws_secret_access_key=aws_secret_key,
     region_name=region_name,
 )
-with open("url_tsv.yml") as url_file:
-    urls = yaml.safe_load(url_file)
 
-# print(urls)
+print("Loading TSV files list")
+with open(TSV_LIST_FILENAME) as url_file:
+    tsv_files = yaml.safe_load(url_file)
 
-for u in urls["sources"]:
-    source_url = u["url"]
-    file_name = u["filename"]
-    from_url = requests.get(source_url)
-    object_url = BytesIO(from_url.content)
-    with gzip.open(object_url, "rt", encoding="utf-8") as gz_file:
-        file_open = pd.read_csv(gz_file, delimiter="\t")
-        file_open.to_csv(file_name, sep="\t", index=False, header=True)
+
+print("Processing TSV requests")
+for tsv_file in tsv_files["sources"]:
+    source_url = tsv_file["url"]
+    file_name = tsv_file["filename"]
+    print(f"Processing URL {source_url}")
+    file_request = requests.get(source_url)
+    remote_file = BytesIO(file_request.content)
+
+    print("Opening compressed file")
+    with gzip.open(remote_file, "rt", encoding="utf-8") as gz_file:
+        print("Loading TSV to Pandas")
+        df = pd.read_csv(gz_file, delimiter="\t")
+        print("Converting to CSV")
+        df.to_csv(file_name, sep="\t", index=False, header=True)
+        print("Uploading to AWS")
         s3.upload_file(file_name, bucket_name, file_name)
